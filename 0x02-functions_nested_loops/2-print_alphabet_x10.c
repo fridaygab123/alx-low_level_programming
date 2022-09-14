dca5593e64b7950a41684eb4b14ec5b4ebf6ab44
fridaygab123 <fridaygabriel962@gmail.com>
2022-09-14 07:05:24 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* print_alphabets -> prints the lowercase alphabets
+* print_alphabets - prints the alphabets 10 times
 
-	Return: Always 0
+Return: Always 0
 */
 void print_alphabet_x10(void)
 {
