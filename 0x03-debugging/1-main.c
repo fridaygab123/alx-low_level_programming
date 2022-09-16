364264a1a465dbda66f7818b6a598802f3243a68
fridaygab123 <fridaygabriel962@gmail.com>
2022-09-16 14:06:22 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - causes an infinite loop
  * Return: 0
@@ -17,9 +16,9 @@ int main(void)
 	/*while (i < 10)*/
 	/*{*/
 		/*putchar(i);*/
-         /* }*/
+         /*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
-	return (0);
+return (0);
 }
