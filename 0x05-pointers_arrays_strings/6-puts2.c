ce5d8ccef7ece864fb19d206a9e0dc1f8f040e5c
fridaygab123 <fridaygabriel962@gmail.com>
2022-09-20 21:20:39 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints only one character
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i strr[i] != '\0'; 1++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
